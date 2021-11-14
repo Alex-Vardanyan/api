cd1f1fbf587efa1b63cf47ee5b130f0d373ad3f2
Alex-Vardanyan <88556305+Alex-Vardanyan@users.noreply.github.com>
2021-11-14 18:01:44 +0400

    Add files via upload

---
+++ Supermarket/Supermarket.Dal/EfStructures/SpecificationEvaluator.cs
@@ -0,0 +1,28 @@
+﻿using Supermarket.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Supermarket.Models.Specifications;
+using Microsoft.EntityFrameworkCore;
+
+namespace Supermarket.Dal.EfStructures
+{
+    public class SpecificationEvaluator<TEntity> where TEntity : BaseEntity
+    {
+        public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> inputQuery, ISpecification<TEntity> spec)
+        {
+            var query = inputQuery;
+
+            if(spec.Criteria != null)
+            {
+                query = query.Where(spec.Criteria); // x => x.SupplierId == id
+            }
+
+            query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
+
+            return query;
+        }
+    }
+}
